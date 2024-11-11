fd61087d40c4fba3f0fb08bedb40a4dc57e31f08
wangyangbin <wybdream@163.com>
2024-11-11 11:28:52 +0800

    fix:删除字体引入

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,17 +1,19 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+/*
+ * @Author: wangyangbin wybdream@163.com
+ * @Date: 2024-11-07 09:51:23
+ * @LastEditors: wangyangbin wybdream@163.com
+ * @LastEditTime: 2024-11-11 11:27:26
+ * @FilePath: \nextjs-dashboard\app\ui\dashboard\cards.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline'
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
-  invoices: InboxIcon,
-};
+  invoices: InboxIcon
+}
 
 export default async function CardWrapper() {
   return (
@@ -27,19 +29,19 @@ export default async function CardWrapper() {
         type="customers"
       /> */}
     </>
-  );
+  )
 }
 
 export function Card({
   title,
   value,
-  type,
+  type
 }: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  title: string
+  value: number | string
+  type: 'invoices' | 'customers' | 'pending' | 'collected'
 }) {
-  const Icon = iconMap[type];
+  const Icon = iconMap[type]
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
@@ -48,11 +50,11 @@ export function Card({
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
+        className={`
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
     </div>
-  );
+  )
 }
--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,36 +1,35 @@
-import { clsx } from 'clsx';
-import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+/*
+ * @Author: wangyangbin wybdream@163.com
+ * @Date: 2024-11-07 09:51:23
+ * @LastEditors: wangyangbin wybdream@163.com
+ * @LastEditTime: 2024-11-11 11:28:11
+ * @FilePath: \nextjs-dashboard\app\ui\invoices\breadcrumbs.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { clsx } from 'clsx'
+import Link from 'next/link'
 
 interface Breadcrumb {
-  label: string;
-  href: string;
-  active?: boolean;
+  label: string
+  href: string
+  active?: boolean
 }
 
-export default function Breadcrumbs({
-  breadcrumbs,
-}: {
-  breadcrumbs: Breadcrumb[];
-}) {
+export default function Breadcrumbs({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
+      <ol className={clsx('flex text-xl md:text-2xl')}>
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
             aria-current={breadcrumb.active}
-            className={clsx(
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
-            )}
+            className={clsx(breadcrumb.active ? 'text-gray-900' : 'text-gray-500')}
           >
             <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
-            {index < breadcrumbs.length - 1 ? (
-              <span className="mx-3 inline-block">/</span>
-            ) : null}
+            {index < breadcrumbs.length - 1 ? <span className="mx-3 inline-block">/</span> : null}
           </li>
         ))}
       </ol>
     </nav>
-  );
+  )
 }
